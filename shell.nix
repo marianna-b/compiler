08fa76b83ba247cbcddd0ad0dea64a38bb27a3f4
mariashka <bisyarinamariashka@gmail.com>
2016-12-21 01:24:41 +0300

    Add llvm-general to dependencies

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 with import <nixpkgs> { };
 let
-  hsPkgs = haskell.packages.ghc801;
+  hsPkgs = haskell.packages.ghc7103;
 in
   haskell.lib.buildStackProject {
      name = "compiler";
      ghc = hsPkgs.ghc;
-     buildInputs = [ git cabal-install stack ];
+     buildInputs = [ git cabal-install stack
+                     llvm_35 ];
      LANG = "en_US.UTF-8";
   }
